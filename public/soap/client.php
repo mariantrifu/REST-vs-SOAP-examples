a84e2e6dfe4eb9755dc50440a4ec4865c15f6331
Marian Trifu <mariantrifu@gmail.com>
2019-06-05 15:49:09 +0300

    update soap client

--- public/soap/client.php
+++ public/soap/client.php
@@ -4,7 +4,7 @@ $options = [
     'location' => 'http://127.0.0.1:8000/soap/server.php',
     'uri' => 'http://127.0.0.1:8000/soap/server.php',
     'trace' => true,
-    'compression'   => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP | SOAP_COMPRESSION_DEFLATE,
+    'compression'   => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
 ];
 
 $client = new SoapClient(NULL, $options);
@@ -12,6 +12,10 @@ $client = new SoapClient(NULL, $options);
 try{
     $products = $client->__soapCall('getProducts', []);
     echo '<pre>';
+    echo htmlentities($client->__getLastRequest());
+    echo '<br/>';
+    echo htmlentities($client->__getLastResponse());
+    echo '<br/>';
     var_dump($products);
 }
 catch (SoapFault $fault){
